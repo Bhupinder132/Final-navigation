29ca548046fcb5dd2b6f17c9cbee8956ef84540d
Bhupinder132 <93252644+Bhupinder132@users.noreply.github.com>
2022-03-17 18:49:11 +0000

    Final

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,43 @@
+export const MenuItems =[
+    {
+        title: 'Home',
+        url:'#',
+        cName: 'nav-links'
+
+    },    {
+        title: 'Activities',
+        url:'#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'Blog',
+        url:'#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'Chat-Platfrom',
+        url:'#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'Location',
+        url:'#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'Admin',
+        url:'#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
